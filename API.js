0cdb1256380ff2b13be342490088c81666fd65f9
lxvc <403381161@qq.com>
2016-07-09 23:54:40 +0800

    修改 API 文档

--- API.js
+++ API.js
@@ -1,12 +1,16 @@
-{
-  "name": "示例",
-  "url": "http://127.0.0.1:5000",
-  "method": "GET",
-  "args": {
-    "id": 1
+const example = {
+  url: "http://127.0.0.1:5000", // 请求地址
+  method: "GET", // 请求方式['GET', 'POST']
+  args: {
+    // GET请求时带的参数， POST 时即为 body
+    id: 1
   },
-  "response": {
-    "id": 1,
-    "name": "语文"
+  response: {
+    // 后端返回值
+    ok: true,
+    error_code: 1024,   // 1000至2000之间
+    error_desc: "密码不正确", // 错误说明
+    id: 1,
+    name: "语文"
   }
 }
