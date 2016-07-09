9c760937fd687e39ea963871cbc730e576cd3bfc
lxvc <403381161@qq.com>
2016-07-10 01:29:31 +0800

    配置了一键部署脚本

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+from fabric.api import run, env, cd, local
+from fabric.colors import green
+import config
+
+env.hosts = config.evn['hosts']
+env.password = config.evn['password']
+
+inter_path = '/home/ubuntu/django_inter/bin/'
+app_path = '/home/ubuntu/jiu_school_back_end'
+host = '0.0.0.0:8000'
+
+def update():
+    local('git push', capture=False, shell=None)
+    with cd(app_path):
+        try:
+            run('pkill python')
+        finally:
+            run('git pull')
+            run(inter_path + 'pip install' + ' -r requirement.txt')
+            run('nohup ' + inter_path + 'python '  + app_path + '/school/manage.py' +' runserver ' + host)
