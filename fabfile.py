b1121c710212a1f39b7de4cce7a9198d990e6ef1
lxvc <403381161@qq.com>
2016-07-11 00:53:30 +0800

    修改脚本适应清除线上数据

--- fabfile.py
+++ fabfile.py
@@ -10,13 +10,18 @@ inter_path = '/home/ubuntu/django_inter/bin/'
 app_path = '/home/ubuntu/jiu_school_back_end/'
 host = '0.0.0.0:8000'
 
-def update():
+def update(drop_data=None):
     local('git push', capture=False, shell=None)
     with cd(app_path):
         try:
             run('pkill python')
         finally:
             run('git pull')
+            if drop_data is not None:
+                run('mysql -u root')
+                run('DROP DATABASE jiu_school;')
+                run('CREATE DATABASE jiu_school;')
+                run('quit;')
             run(inter_path + 'pip install' + ' -r requirement.txt')
             run(inter_path + 'python '  + app_path + 'src/manage.py' + ' migrate')
             run('nohup ' + inter_path + 'python '  + app_path + 'src/manage.py' +' runserver ' + host)
